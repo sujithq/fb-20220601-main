e078618c9e119c2f7e659220f849cd36f7fa81bd
Sujith Quintelier <sujith.quintelier@gmail.com>
2022-06-01 19:02:00 +0000

    draft

---
+++ main.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=3.7.0"
+    }
+  }
+}
+
+module rg {
+    source = "git::https://github.com/sujithq/fb-20220601-module.git//modules/rg"
+}
